64d110ae058bcb2a9f19a915547cec239e747f8e
zuri <zuri@time2split.net>
2024-03-26 01:48:25 +0000

    refactor: drop `FIO` and split `IO` into new `CharPredicates` and `Streams`

--- src/IO.php
+++ src/IO.php
@@ -1,6 +1,8 @@
 <?php
 namespace Time2Split\Help;
 
+use Time2Split\Help\Classes\NotInstanciable;
+
 /**
  * Functions for inputs/outputs.
  *
@@ -8,7 +10,7 @@ namespace Time2Split\Help;
  */
 final class IO
 {
-    use Classes\NotInstanciable;
+    use NotInstanciable;
 
     public static function olderThan(string $a, string $b)
     {
@@ -35,6 +37,7 @@ final class IO
     }
 
     // ========================================================================
+
     public static function printPHPFile(string $path, $data, bool $compact = false)
     {
         $s = var_export($data, true);
@@ -46,6 +49,7 @@ final class IO
     }
 
     // ========================================================================
+
     private static $wdStack = [];
 
     public static function wdPush(string $path): void
@@ -73,6 +77,7 @@ final class IO
     }
 
     // ========================================================================
+
     public static function scandirNoPoints(string $path, bool $getPath = false): array
     {
         $ret = \array_filter(\scandir($path), fn ($f) => $f[0] !== '.');
@@ -85,6 +90,7 @@ final class IO
     }
 
     // ========================================================================
+
     public static function get_ob(\Closure $f): string
     {
         \ob_start();
@@ -92,51 +98,6 @@ final class IO
         return \ob_get_clean();
     }
 
-    public static function stringToStream(string $text = "")
-    {
-        $stream = \fopen('php://memory', 'r+');
-        \fwrite($stream, $text);
-        \rewind($stream);
-        return $stream;
-    }
-
-    public static function isSeekableStream($stream, array $meta_data = null): bool
-    {
-        $meta_data ??= \stream_get_meta_data($stream);
-
-        return $meta_data['seekable'];
-    }
-
-    public static function isReadableStream($stream, array $meta_data = null): bool
-    {
-        $meta_data ??= \stream_get_meta_data($stream);
-        $mode = $meta_data['mode'];
-
-        return \str_starts_with($mode, 'r') || \str_contains($mode, '+');
-    }
-
-    public static function readableStream($stream, bool $rewind = true)
-    {
-        if (\is_string($stream))
-            return self::stringToStream($stream);
-
-        $meta_data ??= \stream_get_meta_data($stream);
-
-        if (\is_resource($stream) && \get_resource_type($stream) === 'stream' && self::isReadableStream($stream, $meta_data)) {
-
-            if ($rewind) {
-
-                if (!self::isSeekableStream($stream, $meta_data))
-                    throw new \Exception("Is not a seekable stream: " . $meta_data['stream_type']);
-
-                \rewind($stream);
-            }
-            return $stream;
-        }
-        throw new \Exception("Cannot make as a readable stream: " . print_r($stream, true));
-    }
-
-    // ========================================================================
     public static function simpleExec(string $cmd, &$output, &$err, ?string $input = null): int
     {
         $parseDesc = fn ($d) => \in_array($d, [
---
+++ src/Streams.php
@@ -0,0 +1,224 @@
+<?php
+namespace Time2Split\Help;
+
+use Time2Split\Help\Classes\NotInstanciable;
+
+/**
+ * Functions for File Inputs/Outputs.
+ * 
+ * @author Olivier Rodriguez (zuri)
+ */
+final class Streams
+{
+    use NotInstanciable;
+
+    /**
+     * Get a stream from a string.
+     */
+    public static function stringToStream(string $text = '', string $mode = 'r+')
+    {
+        $stream = \fopen('php://memory', $mode);
+        \fwrite($stream, $text);
+        \rewind($stream);
+        return $stream;
+    }
+
+    /**
+     * Check if a stream is seekable.
+     * 
+     * @param mixed $stream A resource stream.
+     * @param null|array $meta_data The meta data of the stream obtained from \stream_get_meta_data().
+     * @return bool true if the \fseek() function may be used on the stream.
+     */
+    public static function isSeekableStream($stream, array $meta_data = null): bool
+    {
+        $meta_data ??= \stream_get_meta_data($stream);
+        return $meta_data['seekable'];
+    }
+
+    /**
+     * Check if a stream is readable.
+     * 
+     * @param mixed $stream A resource stream.
+     * @param null|array $meta_data The meta data of the stream obtained from \stream_get_meta_data().
+     * @return bool true if the stream is readable.
+     */
+    public static function isReadableStream($stream, array $meta_data = null): bool
+    {
+        $meta_data ??= \stream_get_meta_data($stream);
+        $mode = $meta_data['mode'];
+
+        return \str_starts_with($mode, 'r') || \str_contains($mode, '+');
+    }
+
+    /**
+     * Ensure that a stream is readable.
+     * 
+     * @param string|resource $stream A stream or a string.
+     * @param bool $rewind true if the returned stream must be rewind.
+     * @throws \Exception If cannot make a readable stream.
+     * @return resource The stream if it is a readable stream, or self::stringToStream($stream) if it is a string. 
+     */
+    public static function readableStream($stream, bool $rewind = true)
+    {
+        if (\is_string($stream))
+            return self::stringToStream($stream);
+
+        $meta_data ??= \stream_get_meta_data($stream);
+
+        if (
+            \is_resource($stream)
+            && \get_resource_type($stream) === 'stream'
+            && self::isReadableStream($stream, $meta_data)
+        ) {
+
+            if ($rewind) {
+
+                if (!self::isSeekableStream($stream, $meta_data))
+                    throw new \Exception("Is not a seekable stream: " . $meta_data['stream_type']);
+
+                \rewind($stream);
+            }
+            return $stream;
+        }
+        throw new \Exception("Cannot make it as a readable stream: " . print_r($stream, true));
+    }
+
+    // ========================================================================
+
+    /**
+     * Skip some characters from a stream according to a predicate.
+     * 
+     * @param mixed $stream A stream.
+     * @param \Closure $predicate A predicate that return true if its input character must be read.
+     * @return int The number of read character.
+     */
+    public static function streamSkipChars($stream, \Closure $predicate): int
+    {
+        return self::skipChars(
+            fn () => \fgetc($stream),
+            fn () => \fseek($stream, -1, SEEK_CUR),
+            $predicate,
+        );
+    }
+    /**
+     * Skip some characters from a stream until a predicate is true.
+     * 
+     * @param mixed $stream A stream.
+     * @param \Closure $predicate A predicate that return true if its input character must not be read.
+     * @return int The number of read character.
+     */
+    public static function streamSkipCharsUntil($stream, \Closure $predicate): int
+    {
+        return \strlen(self::streamGetCharsUntil($stream, $predicate));
+    }
+
+    /**
+     * Get some characters from a stream according to a predicate.
+     * 
+     * @param mixed $stream A stream.
+     * @param \Closure $predicate A predicate that return true if its input character must be read.
+     * @return string|null A string containing the read characters, or null if none is read.
+     */
+    public static function streamGetChars($stream, \Closure $predicate): string
+    {
+        return self::getChars(
+            fn () => \fgetc($stream),
+            fn () => \fseek($stream, -1, SEEK_CUR),
+            $predicate,
+        );
+    }
+
+    /**
+     * Get some characters from a stream until a character or a predicate is true.
+     * 
+     * @param mixed $stream A stream.
+     * @param \Closure|string $endDelimiter A predicate closure that return true if its input character must not be read, or a character that must end the reading when encountered in the stream.
+     * @return string|null A string containing the read characters, or null if none is read.
+     */
+    public static function streamGetCharsUntil($stream, \Closure $endDelimiter): string
+    {
+        return self::getCharsUntil(
+            fn () => \fgetc($stream),
+            fn () => \fseek($stream, -1, SEEK_CUR),
+            $endDelimiter,
+        );
+    }
+
+    /**
+     * Summary of streamUngetc
+     * @param mixed $stream
+     * @return bool
+     * @throws \DomainException if $nb is not positive.
+     */
+    public static function streamUngetc($stream, int $nb = 1): bool
+    {
+        if ($nb < 0)
+            throw new \DomainException("\$nb must be positive, have $nb");
+
+        return \fseek($stream, -$nb, SEEK_CUR);
+    }
+
+    // ========================================================================
+    public static function skipChars(\Closure $fgetc, \Closure $fungetc, \Closure $predicate): int
+    {
+        $nb = 0;
+
+        while (false !== ($c = $fgetc()) && $predicate($c))
+            $nb++;
+
+        if ($c !== false)
+            $fungetc();
+
+        return $nb;
+    }
+
+    public static function getChars(\Closure $fgetc, \Closure $fungetc, \Closure $predicate): string
+    {
+        $ret = '';
+
+        while (false !== ($c = $fgetc()) && $predicate($c))
+            $ret .= $c;
+
+        if ($c !== false)
+            $fungetc();
+
+        return $ret;
+    }
+
+
+    public static function skipCharsUntil(\Closure $fgetc, \Closure $fungetc, \Closure $predicate): int
+    {
+        $nb = 0;
+
+        while (true) {
+            $c = $fgetc();
+
+            if ($c === false)
+                return $nb + 1;
+            if ($predicate($c)) {
+                $fungetc();
+                return $nb;
+            }
+            $nb++;
+        }
+    }
+
+
+    public static function getCharsUntil(\Closure $fgetc, \Closure $fungetc, \Closure $predicate): string
+    {
+        $ret = '';
+
+        while (true) {
+            $c = $fgetc();
+
+            if ($c === false)
+                return $ret;
+            if ($predicate($c)) {
+                $fungetc();
+                return $ret;
+            }
+            $ret .= $c;
+        }
+    }
+}
